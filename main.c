3dc230d77ec3201addda0f216d0bb7ac62d14586
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-04-12 21:51:20 +0300

    checking for return char

--- main.c
+++ main.c
@@ -2,22 +2,27 @@
 #include <mem.h>
 
 int main( void ) {
-    char in[20];
-    char in2[20];
-    char out[39];
+    char in[5];
+    char in2[5];
+    char out[11];
 
     fgets(in,20,stdin);
     fgets(in2,20,stdin);
     if(in == NULL){ in[0] = '\0'; }
     if(in2 == NULL){ in2[0] = '\0'; }
-    //snprintf(out, 20, in);
 
-    strncpy(out, in,20);
+
+    snprintf(out, 20, in);
+    //strncpy(out, in,20);
     int index = strnlen(in, 20);
+    if (out[index] == '\n'){
+        index--;
+    }
     for (int j = 0; j < strnlen(in2, 20); ++j) {
         out[index] = in2[j];
+        printf("the letter is: %c\n", out[index]);
         index = index + 1;
     }
-    printf(out);
+    printf("%c", out);
     return 0;
    }
