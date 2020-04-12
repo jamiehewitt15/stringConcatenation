fad0d370fcedb86e8e39a8cb041b0538f73db88e
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-04-12 21:20:41 +0300

    string concatenation working

--- main.c
+++ main.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
+#include <mem.h>
 
 int main( void ) {
-    int buffer[20];
-    char in = fgets(buffer,20,stdin);
-    char in2 = fgets(buffer,20,stdin);
-    char out;
-    if(in == NULL){
-        buffer[0] = '\0';
-    }
-    if(in2 == NULL){
-        buffer[0] = '\0';
-    }
+    char in[20];
+    char in2[20];
+    char out[39];
 
-    strcpy(out,i n);
-    int index = strlen(in);
+    fgets(in,20,stdin);
+    fgets(in2,20,stdin);
+    if(in == NULL){ in[0] = '\0'; }
+    if(in2 == NULL){ in2[0] = '\0'; }
+    //snprintf(out, 20, in);
 
-    while(index < 39) {
-        out[index] = in2[index-strlen(in)];
+    strncpy(out, in,20);
+    int index = strnlen(in, 20);
+    for (int j = 0; j < strnlen(in2, 20); ++j) {
+        out[index] = in2[j];
         index = index + 1;
-        }
+    }
+    printf(out);
     return 0;
    }
