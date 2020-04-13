fa536faaddbcbdca9ef1e6ca7d5c4e791b5f437c
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-04-13 13:19:52 +0300

    removing the return character

--- main.c
+++ main.c
@@ -1,13 +1,25 @@
+///*** Question 1 ***///
+
 #include <stdio.h>
 #include <mem.h>
 
 int main( void ) {
     char in[20];
     char in2[20];
-    char out[39];
+    char out[40];
 
     fgets(in,20,stdin);
+    for(int x=0; x<20; x++){
+        if (in[x] =='\n'){
+            in[x] = '\0';
+        }
+    }
     fgets(in2,20,stdin);
+    for(int y=0; y<20; y++){
+        if (in2[y] =='\n'){
+            in2[y] = '\0';
+        }
+    }
     if(in == NULL){ in[0] = '\0'; }
     if(in2 == NULL){ in2[0] = '\0'; }
 
