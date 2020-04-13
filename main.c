5139959d2fc124c00c21c6b5c0b2d4165bfc4eff
Jamie Hewitt <jamie.hewitt15@gmail.com>
2020-04-13 12:33:23 +0300

    changed printf from char to string

--- main.c
+++ main.c
@@ -2,9 +2,9 @@
 #include <mem.h>
 
 int main( void ) {
-    char in[5];
-    char in2[5];
-    char out[11];
+    char in[20];
+    char in2[20];
+    char out[39];
 
     fgets(in,20,stdin);
     fgets(in2,20,stdin);
@@ -15,14 +15,13 @@ int main( void ) {
     snprintf(out, 20, in);
     //strncpy(out, in,20);
     int index = strnlen(in, 20);
-    if (out[index] == '\n'){
-        index--;
-    }
+//    if (out[index] == '\n'){
+//        index--;
+//    }
     for (int j = 0; j < strnlen(in2, 20); ++j) {
         out[index] = in2[j];
-        printf("the letter is: %c\n", out[index]);
         index = index + 1;
     }
-    printf("%c", out);
+    printf("%s", out);
     return 0;
    }
